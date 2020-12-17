45b7641379c6ce83ed8bf91470f624b5586df505
KILLER <isphan111@gmail.com>
2020-12-17 19:11:55 +0600

    header

--- dr/settings.py
+++ dr/settings.py
@@ -147,8 +147,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, './frontend/build/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
-
+# STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
