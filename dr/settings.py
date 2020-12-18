c92062a5f7b8cebbc00dbb0955205ebe12093a7d
KILLER <isphan111@gmail.com>
2020-12-18 14:35:45 +0600

    goa

--- dr/settings.py
+++ dr/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku as django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,15 +139,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+django_heroku.settings(locals())
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, './frontend/build/static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, './frontend/build/static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
--- frontend/views.py
+++ frontend/views.py
@@ -3,7 +3,7 @@ from django.http.response import HttpResponse,HttpResponseNotFound
 from django.views import View
 
 def index(request):
-    return HttpResponse("<h1>Myupwork Backend<h1>")
+    return render(request,'build/index.html')
 
 # class Assets(View):
     
